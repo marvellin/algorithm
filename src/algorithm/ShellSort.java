58c43a0d98b573cf91a7486b65b0d87902a6edee
marvellin <1021478667@qq.com>
2020-08-17 18:02:36 +0800

    0817 改进希尔排序

--- src/algorithm/ShellSort.java
+++ src/algorithm/ShellSort.java
@@ -9,11 +9,23 @@ public class ShellSort implements Solution {
 	public int[] solution(int [] array) {
 		// TODO Auto-generated method stub
 		FileIO.writeArray(array,"shellSort.txt");
-		int dk = array.length / 2;
+		for (int dk = array.length / 2; dk > 0; dk/=2){
+			for (int i = dk; i < array.length; i++){
+				for (int j = i; j > dk - 1; j-=dk){
+					if (array[j] < array[j-dk]){
+						int tmp = array[j];
+						array[j] = array[j-dk];
+						array[j-dk] = tmp;
+						FileIO.writeArray(array,"shellSort.txt");
+					}
+				}
+			}
+		}
+		/*int dk = array.length / 2;
 		while (dk >= 1){
 			shellSort(array, dk);
 			dk /= 2;
-		}
+		}*/
 		return array;
 	}
 
